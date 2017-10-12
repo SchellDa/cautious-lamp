de4a754ab904bc6fd070ead0a25430310f9d3128
Daniel Schell <daniel.schell@kit.edu>
2017-10-12 08:02:50 +0200

    Add correlation plots of missing telescope planes

--- alibavacorrelator.cpp
+++ alibavacorrelator.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include <TFile.h>
 #include <TTree.h>
+#include <TCanvas.h>
+#include <TImage.h>
 #include <TH2F.h>
 
 #include "boost/program_options.hpp"
@@ -12,15 +16,20 @@
 #define MIMOSA_N_Y 576
 #define ALIBAVA_N 256
 
+void correlateAlibavaToTelescope(AlibavaData* aliData, 
+				 TelescopePlaneClusters* planeData, 
+				 TH2F* corX, TH2F* corY);
+
 int main(int argc, char** argv){
 
+	// parse command line arguments to BOOST
 	namespace po = boost::program_options;
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help,h", "Print help message")
 		("file,f", po::value<std::string>()->required(),
 		 "Define input ROOT file")
-		("out,o", po::value<std::string>()->default_value("out.root"),
+		("out,o", po::value<std::string>()->default_value("out"),
 		 "Define ROOT output file name");
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -31,7 +40,7 @@ int main(int argc, char** argv){
 	}
 	po::notify(vm);
 	
-	// Read in
+	// Read input file 
 	TFile *inFile = new TFile(vm["file"].as<std::string>().c_str(), "READ");
 	TTree *inTree = nullptr;
 	inFile->GetObject("data", inTree);
@@ -41,39 +50,92 @@ int main(int argc, char** argv){
 		std::cerr << "ROOT tree not found in file" << std::endl;
 	}
 
+	// Initialize data container and link them to their corresponding 
+	// branch
 	TelescopeData *telData = new TelescopeData;
 	AlibavaData *aliData = new AlibavaData;
 	inTree->SetBranchAddress("telescope", &telData);
 	inTree->SetBranchAddress("alibava", &aliData);
 
+	// Initialize output file and also create and switch to first folder
 	TFile *oFile = new TFile((vm["out"].as<std::string>()+".root").c_str(), 
 				 "RECREATE");
-	TH2F *corX = new TH2F("xcor", "X correlation", 
-			      ALIBAVA_N, 0, ALIBAVA_N, 
-			      MIMOSA_N_X, 0, MIMOSA_N_X);
-	TH2F *corY = new TH2F("ycor", "Y correlation", 
-			      ALIBAVA_N, 0, ALIBAVA_N,
-			      MIMOSA_N_Y, 0, MIMOSA_N_Y);
-
 	oFile->mkdir("ali_tel_cor");
 	oFile->cd("ali_tel_cor");
+
+	// Prepare all histograms
+	std::vector<TH2F*> aliTelX;
+	std::vector<TH2F*> aliTelY;
 	
+	for(size_t iPlane = 0; iPlane < 6; ++iPlane)
+	{
+		std::ostringstream name;
+		std::ostringstream descr;
+		name << "plane_" << iPlane << "_";
+		descr << "Plane " << iPlane << " <-> Alibava correlation in ";
+
+		auto *corX = new TH2F((name.str()+"X").c_str(), 
+				      (descr.str()+"X").c_str(),
+				      ALIBAVA_N, 0, ALIBAVA_N, 
+				      MIMOSA_N_X, 0, MIMOSA_N_X);
+		auto *corY = new TH2F((name.str()+"Y").c_str(), 
+				      (descr.str()+"Y").c_str(),				   
+				      ALIBAVA_N, 0, ALIBAVA_N,
+				      MIMOSA_N_Y, 0, MIMOSA_N_Y);
+		aliTelX.push_back(corX);
+		aliTelY.push_back(corY);
+	}
+
+	// Loop over all events and fill the histograms
 	for(int iEvt = 0; iEvt < inTree->GetEntries(); ++iEvt)
 	{
-
 		inTree->GetEvent(iEvt);
 
-		for(int tp = 0; tp < telData->p3.x.GetNoElements(); ++tp) {
-			for(size_t ap = 0; ap < aliData->center.GetNoElements(); ++ap) {
-				corX->Fill(telData->p3.x[tp], aliData->center[ap]);
-				corY->Fill(telData->p3.y[tp], aliData->center[ap]);
-			}
+		for(size_t telIdx = 0; telIdx < 6; ++telIdx) {
+			auto td = (&telData->p1) + telIdx;
+			correlateAlibavaToTelescope(aliData, td, 
+						    aliTelX[telIdx], 
+						    aliTelY[telIdx]);
 		}
 	}
+
+	// Create summery plot
+	TCanvas *canvas = new TCanvas("c1", "Correlation Plots", 1920, 1080);
+	canvas->Divide(3,2);
+
+	for(size_t telIdx = 0; telIdx < 6; ++telIdx){
+		canvas->cd(telIdx+1);
+		aliTelX[telIdx]->Draw("colz");
+	}
 	
+	auto img = TImage::Create();
+	img->FromPad(canvas);
+	img->WriteImage((vm["out"].as<std::string>()+".png").c_str());
+	delete img;
+
 	inFile->Close();
 	oFile->Write();
 	oFile->Close();
 
+
 	return 0;
 }
+
+void correlateAlibavaToTelescope(AlibavaData* aliData, 
+				 TelescopePlaneClusters* planeData, 
+				 TH2F* corX, TH2F* corY)
+{
+	for(int iEvt = 0; iEvt < planeData->x.GetNoElements(); ++iEvt)
+	{
+		//inTree->GetEvent(iEvt);
+		for(int tp = 0; tp < planeData->x.GetNoElements(); ++tp) {
+			for(int ap = 0; ap < aliData->center.GetNoElements(); ++ap) {
+				corX->Fill(aliData->center[ap], planeData->x[tp]);
+				corY->Fill(aliData->center[ap], planeData->y[tp]);
+			}
+		}
+	}
+}
+
+
+ 
